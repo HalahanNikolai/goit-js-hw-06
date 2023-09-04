11d2aba66e20bdd7c35cab176a2fa899fab2b832
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2023-09-04 19:22:13 +0300

    Task_9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+//! ----------------------------------------------------------
+const page = document.querySelector('body')
+const btn = document.querySelector('button.change-color')
+const colorVal = document.querySelector('span.color')
+btn.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  page.style.backgroundColor = color;
+  colorVal.textContent = color;
+})
