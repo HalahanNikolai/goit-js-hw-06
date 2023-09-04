f0e440137509bf57a6f3b1607e9d8c34758b29bf
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2023-09-04 19:04:36 +0300

    Task_8(7)

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const logForm = document.querySelector('.login-form')
 
+logForm.addEventListener('submit', submitHandler)
+
+function submitHandler(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!')
+    }
+    const userDetails = { email: email.value, Password: password.value }
+
+    console.log(userDetails)
+    event.currentTarget.reset()
+}
